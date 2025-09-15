5571e5c82c87933fdcf388fabcd2418180a00710
mobin <mobin0263@gmail.com>
2025-09-15 19:04:10 +0200

    Initial Commit

---
+++ DayCounter/DayCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayCounterApp.swift
+//  DayCounter
+//
+//  Created by Mobin on 15.09.25.
+//
+
+import SwiftUI
+
+@main
+struct DayCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
